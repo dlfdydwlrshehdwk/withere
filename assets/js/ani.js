2cdb4e5d8a821c967345bcbb41fc194e626aba4c
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-08-16 17:40:29 +0900

    1

--- assets/js/ani.js
+++ assets/js/ani.js
@@ -1,10 +1,21 @@
 let ani = {
   init: function () {
   }
+
+
 };
 
 $(function () {
  
     ani.init();
- 
+
+    $('.toggle').slideup()
+    $('.toggle')
+    .hover(
+    function(){
+
+    },
+    function(){
+
+    })
 });
