73905e0cfc0cce8001997e3a113be3de0fa02650
dlfdydwlrshehdwk <fkrmsk147@naver.com>
2023-08-17 01:47:39 +0900

    1

--- assets/js/ani.js
+++ assets/js/ani.js
@@ -10,12 +10,19 @@ $(function () {
     ani.init();
 
     // $('.sub-menu').hide()
-    // $('.toggle')
-    // .hover(
-    // function(){
-    //   $('.sub-menu').stop().slideDown()
-    // },
-    // function(){
-    //   $('.sub-menu').stop().slideUp()
-    // })
+
+    // nav의 각 서브메뉴와의 길이를 맞추기위해 
+    $('.main-menu').eq(1).css({width : $('.sub-menu').eq(0).width() + 'px'})
+    $('.main-menu').eq(2).css({width : $('.sub-menu').eq(1).width() + 'px'})
+    $('.main-menu').eq(3).css({width : $('.sub-menu').eq(2).width() + 'px'})
+    $('.main-menu').eq(4).css({width : $('.sub-menu').eq(3).width() + 'px'})
+    $('.main-menu').eq(5).css({width : $('.sub-menu').eq(4).width() + 'px'})
+
+    $('.nav').hover(
+    function(){
+      $('.sub-menu').stop().show()
+    },
+    function(){
+      $('.sub-menu').stop().hide()
+    })
 });
