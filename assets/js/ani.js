d596242899ecb093c8cb3922a17b8152838a4db1
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-08-17 16:01:40 +0900

    2

--- assets/js/ani.js
+++ assets/js/ani.js
@@ -9,19 +9,21 @@ $(function () {
  
     ani.init();
 
+    // 네비게이션 초기에 안보이게 선셋팅
     $('.sub-menu').css({
       overflow : 'hidden',
       height : '0'
     })
-
+    // 네비게이션 오버시 
     $('.nav').hover(
     function(){
       $('.sub-menu').css({
         height : 'auto'
       })
       $('.headerwrap').css({height : '272px',
-      backgroundColor : 'white' })
+      backgroundColor : 'beige' })
     },
+    // 네비게이션 오버 뗐을때
     function(){
       $('.sub-menu').css({
         overflow:'hidden',
@@ -30,4 +32,19 @@ $(function () {
       $('.headerwrap').css({height : '100px',
       backgroundColor : 'white' })
     })
+
+    // product부분 초기에 안보이게 셋팅
+    $('.product-bottombx').hide()
+    $('.product-sc').hover(
+      // 오버시
+      function(){
+        console.log('h')
+        $(this).find('.product-bottombx').slideDown()
+      },
+      // 아웃시
+      function(){
+        $(this).find('.product-bottombx').slideUp()
+      })
+
+
 });
