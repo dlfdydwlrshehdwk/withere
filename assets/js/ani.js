e1b1d1a70faa9d726b7fcf7f1c484859638dcfd9
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-08-17 14:56:33 +0900

    2

--- assets/js/ani.js
+++ assets/js/ani.js
@@ -9,20 +9,25 @@ $(function () {
  
     ani.init();
 
-    // $('.sub-menu').hide()
-
-    // nav의 각 서브메뉴와의 길이를 맞추기위해 
-    $('.main-menu').eq(1).css({width : $('.sub-menu').eq(0).width() + 'px'})
-    $('.main-menu').eq(2).css({width : $('.sub-menu').eq(1).width() + 'px'})
-    $('.main-menu').eq(3).css({width : $('.sub-menu').eq(2).width() + 'px'})
-    $('.main-menu').eq(4).css({width : $('.sub-menu').eq(3).width() + 'px'})
-    $('.main-menu').eq(5).css({width : $('.sub-menu').eq(4).width() + 'px'})
+    $('.sub-menu').css({
+      overflow : 'hidden',
+      height : '0'
+    })
 
     $('.nav').hover(
     function(){
-      $('.sub-menu').stop().show()
+      $('.sub-menu').css({
+        height : 'auto'
+      })
+      $('.headerwrap').css({height : '272px',
+      backgroundColor : 'white' })
     },
     function(){
-      $('.sub-menu').stop().hide()
+      $('.sub-menu').css({
+        overflow:'hidden',
+        height : '0'
+      })
+      $('.headerwrap').css({height : '100px',
+      backgroundColor : 'white' })
     })
 });
