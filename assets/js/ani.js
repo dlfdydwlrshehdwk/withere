922a19a0369df9cf7b5cba8a48fdefdcff3b3e4e
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-08-18 10:35:42 +0900

    2

--- assets/js/ani.js
+++ assets/js/ani.js
@@ -53,13 +53,21 @@ $(function () {
       let targetPage = $('.news'); // 타겟박스
       let stickyBx = $('.news-slide') // 스티키 될 박스
       let moveBx = $('.news-slide ul') // 실제로 움직일 박스
-      let point = targetPage.offset().top
       
       $(window).on('scroll',function(){
-        let c = targetPage.offset()
-        let d = $('.news')
-
-        console.log(c,d,e,point)
+        let windowH = $(window).scrollTop()
+        let point = targetPage.offset().top // DOM에서 요소간의 거리 4446
+        let targetP = windowH - point // js에서 getBoundingClientRect() 느낌
+        console.log('스크롤값',windowH,'\n포인트값',point,'사용할숫자\n',targetP)
+        let a = $('.news-content').height()
+        // 가로스크롤처럼 보일 적용구간 
+        if(targetP >= 0 && targetP <= 3480 - a ){
+          console.log('여기') 
+          moveBx.css({left :'-' + targetP + 'px'})
+        }
+        else if(targetP < 0){
+          moveBx.css({left : '0'});
+        }
       })
 
       
