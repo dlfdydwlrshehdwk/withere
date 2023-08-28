9cbbb93d91d61528c8a807feed65fea4c326c327
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-08-28 17:28:40 +0900

    1

--- assets/js/ani.js
+++ assets/js/ani.js
@@ -2,7 +2,6 @@ let ani = {
   init: function () {
     // .scrolltop 을 누르면 맨위로
     $('.scrolltop').click(function(){
-      // location.reload()
       $(window).scrollTop(0)
       $('.headerwrap').css({top:'0'})
       $('#header').css({top:0})
@@ -10,6 +9,11 @@ let ani = {
     let suchi = $('.headerwrap').width();
     $(window).resize(function(){
       suchi = $('.headerwrap').width()
+      if (suchi > 1440) {
+        $('.sub-menu').removeClass('open');
+        initArrow()
+        $('.gnb').removeClass('mobile')
+      }
     })
       // 네비게이션 오버시 
       $('.nav').hover(
@@ -27,8 +31,7 @@ let ani = {
       function(){
         if(suchi > 1440){
           $('.sub-menu').css({
-            overflow:'hidden',
-            height : '0'
+            height : ''
           })
           $('.headerwrap').css({height : '100px'})
         }
@@ -41,47 +44,50 @@ let ani = {
       // 모바일기준 1440px 아래일때 nav 클릭함수
       $('.main-menu').click(function(event){
         initList($(this))
-        initArrow($(this))
+        siblingsInitArrow($(this))
         arrow($(this))
       })
       // 화살표 위로가르키기
-      function arrowUp(a){
-        a.find('.arrow').eq(0).removeClass('show')
-        a.find('.arrow').eq(1).addClass('show')
+      function arrowUp(target){
+        target.find('.arrow').eq(0).removeClass('show')
+        target.find('.arrow').eq(1).addClass('show')
       }
       // 화살표 아래로 가르키기
-      function arrowDown(a){
-        a.find('.arrow').eq(1).removeClass('show')
-        a.find('.arrow').eq(0).addClass('show')
+      function arrowDown(target){
+        target.find('.arrow').eq(1).removeClass('show')
+        target.find('.arrow').eq(0).addClass('show')
       }
       // 화살표 조건에 맞게 분포한 함수
-      function arrow(a){
-        if(a.find('.arrow').eq(0).hasClass('show') == true){
-          arrowUp(a)
-          listDown(a)
+      function arrow(target){
+        if(target.find('.arrow').eq(0).hasClass('show') == true){
+          arrowUp(target)
+          listDown(target)
         }
-        else if(a.find('.arrow').eq(0).hasClass('show') == false){
-          arrowDown(a)
-          listUp(a)
+        else if(target.find('.arrow').eq(0).hasClass('show') == false){
+          arrowDown(target)
+          listUp(target)
         }
       }
       // 리스트 아래로펼치기
-      function listDown(a){
-        a.find('.sub-menu').addClass('open')
+      function listDown(target){
+        target.find('.sub-menu').addClass('open')
       }
       // 리스트 위로 올라오기
-      function listUp(a){
-        a.find('.sub-menu').removeClass('open')
+      function listUp(target){
+        target.find('.sub-menu').removeClass('open')
       }
       // 모든리스트 위로 올라오기
-      function initList(a){
-        a.siblings().find('.sub-menu').removeClass('open')
-
+      function initList(target){
+        target.siblings().find('.sub-menu').removeClass('open')
       }
-      // 모든화살표 초기화
-      function initArrow(a){
-        a.siblings().find('.arrow:odd').removeClass('show')
-        a.siblings().find('.arrow:even').addClass('show')
+      // 누른애 빼고 모든화살표 초기화
+      function siblingsInitArrow(target){
+        target.siblings().find('.arrow:odd').removeClass('show')
+        target.siblings().find('.arrow:even').addClass('show')
+      }
+      function initArrow(){
+        $('.arrow:odd').removeClass('show')
+        $('.arrow:even').addClass('show')
       }
 
     // product부분 초기에 안보이게 셋팅
--- assets/js/scrollMagic.js
+++ assets/js/scrollMagic.js
@@ -1,30 +1,26 @@
 let scrollMagic = {
     init : function(){
         let wH = $(window).height()
-    // 스크롤매직을 이용한 이미지 시퀀스
+        
     function sq(trele,start,end,next){
-    // 트리거요소, 핀요소, 시작사진수,끝사진수
-    // 1. 빈배열을 만들어서 배열안에 이미지 소스들을 넣어줌 for문활용했음
+    // sq(트리거요소, 핀요소, 시작사진수,끝사진수)
     let image = [] 
-    // TweenMax는 모든 객체의 속성을 구분할 수 있다. 이 객체를 사용하여 배열을 순환한다
     let obj = { curimg : 0 }
     for ( let i = start; i < end ; i++ ){
         image.push(`../assets/images/sequence/sequence_${i}.jpg`)
     }
-    // 2. 컨트롤러 생성 - 왜하는지모름
+    // 2. 컨트롤러 생성 
     let controller = new ScrollMagic.Controller();
     
     // 3. 애니메이션 오브젝트생성
-    let tween = TweenMax.to(obj,0.3,{ // 트윈맥스.to( 요소, 시간 ,{ 속성들 }) 근데 여기선 시간 안먹는듯? 다똑같음 스크롤이라그런가
+    let tween = TweenMax.to(obj,0.3,{ // 트윈맥스.to( 요소, 시간 ,{ 속성들 })
         curimg : image.length - 1, // 애니메이트 속성 curImg 이미지 수로 이동 1 - 175
-        roundProps : "curimg", // 배열 인덱스로 사용할 수 있도록 정수만 사용한다는데 뭔지모름
+        roundProps : "curimg", // 배열 인덱스로 사용할 수 있도록 정수만 사용
         repeat :0, // 반복할건지 ? 안할거라 0 할거면 숫자 
         
         immediateRender : false, // 첫번째 이미지 자동 로드할건지?
         ease: Linear.easeNone, // 이징 : 일정하게
-        // offset : wH, // 트리거에서 150px밑에서 실행 애니메이션 지연 느낌 
-        // duration : 200, // pin이용할거면 duration 지정 근데 잘안되는걸로봐서 scene에서 정하는거같음
-        onUpdate : function(){ // 이미지 경로를 업데이트해주나봄 첨봄
+        onUpdate : function(){ // 이미지 경로를 업데이트해줌
             $('.testimg').attr('src',image[obj.curimg])
         },
         onComplete : ()=>{
@@ -39,18 +35,12 @@ let scrollMagic = {
         triggerHook: 0 ,// 트리거 위치 조정 0 맨위 기본값 화면중앙 
         offset : wH,
     })
-    .setTween(tween) // 애니메이션 오브젝트 tween을 장착
-    // .setPin(pinele , {pushFollowers : false}) 
-    // 요소 고정가능하다고함 특정지점이후로는 따라오지않게 하려면 속성에서 duration지정
-    // pushFollowers : false 하면 핀-스페이서 안생긴다고함
+    .setTween(tween)
     .addTo(controller)
     }
     // sq("#trigger1","#pin1",1,176,$('.sqimg1'))
     // sq(트리거요소,핀요소,시작숫자,끝숫자,사진출력할타겟요소)
     function silhum(trigger,startN,endN,next){
-        // 트리거 - 각 화면멈처구간에서 트리거훅 1줘서 맨밑에서실행
-        // onStart로 사진 넣는공간 dib 
-        // onComplete 로 끝날때 dn
         let controller2 = new ScrollMagic.Controller();
             let tween_on = TweenMax.to('.bg-section',0.1,{
                 display : 'block',
@@ -141,7 +131,6 @@ $(window).scroll(function(){
     chgColor($('.t2'),$('.txtwrap2'))
     chgColor($('.t3'),$('.txtwrap3'))
 })
-
     }
 }
 
